772262ad678fcf880b58fe345163cefae608a6b9
Onyx590 <egbe.sardonyx@philander.edu>
2024-11-07 00:12:31 -0500

    first commit

---
+++ src/main/java/delta_synchronous_computer_science_organization/gradetracker_project/GradeTracker_Project.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package delta_synchronous_computer_science_organization.gradetracker_project;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Blackstone
+ */
+
+
+public class GradeTracker_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/delta_synchronous_computer_science_organization/gradetracker_project/Student.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package delta_synchronous_computer_science_organization.gradetracker_project;
+
+/**
+ *
+ * @author Blackstone
+ */
+public class Student {
+    public String first_name;
+    public String last_name;
+    public enum classification{};
+}
