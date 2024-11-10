6f9aa6a8324e4eab0cca4ea68b440d331e5525b4
Onyx590 <egbe.sardonyx@philander.edu>
2024-11-10 02:03:21 -0500

    This is my first implementation of the student class. I have still not decided on access (private vs public). I have also decided to make the Classes class within this class an ArrayList (Could change later).
    We might also add more functionality later.

--- src/main/java/delta_synchronous_computer_science_organization/gradetracker_project/Student.java
+++ src/main/java/delta_synchronous_computer_science_organization/gradetracker_project/Student.java
@@ -4,6 +4,8 @@
  */
 package delta_synchronous_computer_science_organization.gradetracker_project;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Blackstone
@@ -11,7 +13,50 @@ package delta_synchronous_computer_science_organization.gradetracker_project;
 public class Student {
     public String first_name;
     public String last_name;
-    public enum classification{};
-    public Classes studentclass; //We should convert this to a list that is able to grow
-    public Grades studentgrades; //Not sure if this should be here
+    public enum classification{FRESHMAN, SOPHOMORE, JUNIOR, SENIOR};
+    public ArrayList<Classes> student_subjectclass; //We should convert this to a list that is able to grow
+   // public Grades studentgrades; //Not sure if this should be here
+    public classification estudent_class;
+   
+    
+    //constructors
+    public Student(String first_name, String last_name, classification estudent_class, ArrayList<Classes> student_subjectclass){
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.estudent_class = estudent_class;
+        this.student_subjectclass = student_subjectclass;
+    }
+    public Student() {
+        this.first_name = "";
+        this.last_name = "";
+        this.estudent_class = estudent_class.FRESHMAN;
+        this.student_subjectclass = new ArrayList<Classes>();
+    }
+    
+    //setters
+    public void setFname(String fName) {
+        first_name = fName;
+    }
+    
+    public void setLname (String lName) {
+        last_name = lName;
+    }
+    
+    public void setClassification (classification eclass) {
+        estudent_class = eclass;
+    }
+    
+    //getters
+    public String getFname () {
+        return first_name;
+    }
+    
+    public String getLname() {
+        return last_name;
+    }
+    
+    public classification getClassification() {
+        return estudent_class;
+    }
+    
 }
