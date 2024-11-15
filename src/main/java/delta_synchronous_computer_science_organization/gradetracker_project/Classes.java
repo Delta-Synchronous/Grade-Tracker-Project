3fe07a79724a5944e960149a53b17a295b8bd570
KWill15 <152660766+KWill15@users.noreply.github.com>
2024-11-14 23:30:04 -0600

    Added just some basic variables and grabber methods

--- src/main/java/delta_synchronous_computer_science_organization/gradetracker_project/Classes.java
+++ src/main/java/delta_synchronous_computer_science_organization/gradetracker_project/Classes.java
@@ -8,6 +8,53 @@ package delta_synchronous_computer_science_organization.gradetracker_project;
  *
  * @author Blackstone
  */
-public class Classes {
+public class Classes 
+{
+    public String subject;
+    public String section;
+    public double overallGrade; //Need to figure out how to keep track of someones overall grade in a certain class when they have multiple
+    public double assignment;  //This should be added by the user and should influence thier overall grade. Still need to work out stuff like weighting
     
+    public Classes(String sub, String sec, double grade)
+    {
+        subject = sub;
+        section = sec;
+        overallGrade = grade;
+        assignment = 0.0;
+    }
+    
+    public Classes()
+    {
+        subject = "No Subject";
+        section = "No Section";
+        overallGrade = 0.0;
+        assignment = 0.0;
+    }
+    
+    public String getSubject()
+    {
+        return subject;
+    }
+    
+    public String getSection()
+    {
+        return section;
+    }
+    
+    public double getGrade()
+    {
+        return overallGrade;
+    }
+    
+    public double lastAssignment()
+    {
+        return assignment;
+    }
+    
+    public void addClass(String sub, String sec, double grade) //Need to figure out the container to keep track of these
+    {
+        subject = sub;
+        section = sec;
+        overallGrade = grade;
+    }
 }
